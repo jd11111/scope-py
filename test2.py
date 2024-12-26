db4628bb9798399b9ee601356bde59b5a4062d4e
jd11111 <jannik.daun@outlook.de>
2024-12-26 12:51:00 +0000

    finished initial class

--- test2.py
+++ test2.py
@@ -18,7 +18,8 @@ UNIT_INFO_TYPES = {"DriverVersion": 0,
 voltRangeOpts= {20:1,50:2,100:3,200:4,500:5,1000:6,2000:7,5000:8,10000:9,20000:10}
 couplingOpts = {"AC": 0, "DC":1}
 channelOpts = {"A":0, "B" : 1}
-sigOpts = {}
+sigOpts = {"sin":0, "square":1,"triangle":2,"rampup":3, "rampdown":4,"dc":5,"gaussian":6, "sinc":7,"halfsin":8}
+sweepOpts = {"up":0,"down":1,"updown":2,"downup":3}
 
 
 class ps2000():
@@ -68,93 +69,52 @@ class ps2000():
                 self.mss.append(ms)
                 tb+=1
 
-    def sig_gen(self):
+    def sig_gen(self,PkToPk,waveForm,startFreq,stopFreq,offsetV=0,increment=0.0,dwellTime=0.0,sweepType="up",sweeps=0):
+        code = lib.ps2000_set_sig_gen_built_in(c_int16(self.handle),c_int32(offsetV),c_uint32(PkToPk),c_int16(sigOpts[waveForm]),c_float(startFreq),c_float(stopFreq),c_float(increment),c_float(dwellTime),c_int16(sweepOpts[sweepType]),c_uint32(sweeps))
+        if code ==0:
+            raise Exception("signal generation error/parameters out of range")
+        print("signal generation started sucessfully")
 
+    def run_block(self,T):
+        if not self.channelset:
+            raise Exception("can not run block without setting up channel first")
+        for ind, tb in enumerate(self.tbs):
+            if T<(self.tis[ind]).value*(self.mss[ind]).value:
+                besttb = tb
+                bestidx = ind
+                success =True
+                break
+        if not success:
+            raise Expection("Time T is too large")
 
+        code = lib.ps2000_run_block(c_int16(self.handle),self.mss[bestidx],c_int16(besttb),c_int16(1),c_void_p())
+        if code ==0:
+            raise Exception("Error running block/Parameters out of range")
+        while not lib.ps2000_ready(c_int16(self.handle)):
+            time.sleep(0.2)
 
+        dataA = np.zeros((self.mss[bestidx]).value,dtype=np.int16)
+        dataB = np.zeros((self.mss[bestidx]).value,dtype=np.int16)
 
+        dataAPtr = dataA.ctypes.data_as(POINTER(c_int16))
+        dataBPtr= dataB.ctypes.data_as(POINTER(c_int16))
 
+        code = lib.ps2000_get_values(c_int16(self.handle),dataAPtr,dataBPtr,c_void_p(),c_void_p(),c_void_p(),self.mss[bestidx])
+        if code ==0:
+            raise Exception("Error getting values/Parameters out of range")
+        print("run block sucessfull")
 
-
-
-
-
+        return dataA, dataB, (self.tis[bestidx]).value, (self.mss[bestidx]).value
 
 scope = ps2000()
 scope.dump_info()
-scope.set_channel("A",True,100,"DC")
+scope.set_channel("A",True,1000,"DC")
+scope.sig_gen(10**6,"sin",1.43,1.43)
+
+T= 10**9
+dataA, dataB, dt, n = scope.run_block(T)
 scope.close_scope()
-"""
-handle = lib.ps2000_open_unit()
-print(handle)
-info = "DriverVersion"
-lib.ps2000_flash_led(c_int16(handle))
-
-c = lib.ps2000_set_channel(c_int16(handle),c_int16(0), c_int16(1),c_int16(1),c_int16(6))
-print(c)
-
-#Have: fixed time interval T (say period of signal)
-#Want: smallest timebase tb such that ti*maxsamples > T
-#Want: fixed nr of evenly spaced measurements
-#Can ask scope: at timebase x what will be time dt between measurement, and how many measurements can we do (in one block)
-#idea: loop over all timebases, check that we can do enough samples
-tbs =[]
-tis = []
-mss = []
-tb =1
-cond = True
-while cond:
-    ti = c_int32()
-    ms = c_int32()
-    code = lib.ps2000_get_timebase(c_int16(handle),c_int16(tb),c_void_p(),byref(ti),c_void_p(),c_int16(1),byref(ms))
-    if code==0:
-        cond = False
-    else:
-        tis.append(ti)
-        tbs.append(tb)
-        mss.append(ms)
-        tb+=1
-
-print(tbs)
-print(tis)
-print(mss)
-
-T = 10**9
-
-code = lib.ps2000_set_sig_gen_built_in(c_int16(handle),c_int32(0),c_uint32(10**6),c_int16(0),c_float(1.43),c_float(1.43),c_float(0),c_float(0),c_int16(0),c_uint32(0))
-print("code for sig gen")
-print(code)
-
-def run_block(T):
-    for ind, tb in enumerate(tbs):
-        if T<tis[ind].value*mss[ind].value:
-            besttb = tb
-            bestidx = ind
-            success =True
-            break
-    if not success:
-        raise Expection("Time T is too large")
-
-    code = lib.ps2000_run_block(c_int16(handle),mss[bestidx],c_int16(besttb),c_int16(1),c_void_p())
-    while not lib.ps2000_ready(c_int16(handle)):
-        time.sleep(0.2)
-
-    dataA = np.zeros(mss[bestidx].value,dtype=np.int16)
-    dataB = np.zeros(mss[bestidx].value,dtype=np.int16)
-
-    dataAPtr = dataA.ctypes.data_as(POINTER(c_int16))
-    dataBPtr= dataB.ctypes.data_as(POINTER(c_int16))
-
-    code = lib.ps2000_get_values(c_int16(handle),dataAPtr,dataBPtr,c_void_p(),c_void_p(),c_void_p(),mss[bestidx])
-
-    return dataA, dataB, tis[bestidx].value, mss[bestidx].value
-
-dataA, dataB, dt, n = run_block(T)
-
-lib.ps2000_close_unit(c_int16(handle))
 
 fig, ax  = plt.subplots()
 ax.plot(10**-9*dt*np.array(range(n)),dataA/32767)
-
 plt.savefig("test.png")
-    """
