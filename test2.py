73e38375dbe0009aed2b64850c2570f388014e5b
jd11111 <jannik.daun@outlook.de>
2024-12-26 11:49:55 +0000

    started implementing class

--- test2.py
+++ test2.py
@@ -6,9 +6,6 @@ from ctypes import cdll, byref, POINTER, create_string_buffer, c_float, c_int16,
 #from ctypes import c_int32 as c_enum
 
 lib = cdll.LoadLibrary("/home/jd/scope-py/libps2000.so")
-handle = lib.ps2000_open_unit()
-print(handle)
-s = create_string_buffer(256)
 UNIT_INFO_TYPES = {"DriverVersion": 0,
                        "USBVersion": 1,
                        "HardwareVersion": 2,
@@ -17,12 +14,80 @@ UNIT_INFO_TYPES = {"DriverVersion": 0,
                        "CalibrationDate": 5,
                        "ErrorCode": 6,
                        "KernelDriverVersion": 7}
-info = "DriverVersion"
-for info in UNIT_INFO_TYPES.keys():
-    print(info)
-    lib.ps2000_get_unit_info(c_int16(handle), byref(s), c_int16(len(s)), c_int32(UNIT_INFO_TYPES[info]))
-    print(s.value.decode('utf-8'))
 
+voltRangeOpts= {20:1,50:2,100:3,200:4,500:5,1000:6,2000:7,5000:8,10000:9,20000:10}
+couplingOpts = {"AC": 0, "DC":1}
+channelOpts = {"A":0, "B" : 1}
+sigOpts = {}
+
+
+class ps2000():
+    def __init__(self):
+        code =lib.ps2000_open_unit()
+        if code ==0:
+            raise Exception("scope could not be found/accessed")
+        self.handle = code
+        self.channelset =False
+        print("opening scope successful")
+        lib.ps2000_flash_led(c_int16(self.handle))
+
+    def dump_info(self):
+        s = create_string_buffer(256)
+        print("dumping scope info:")
+        for info in UNIT_INFO_TYPES.keys():
+            print(info+":")
+            lib.ps2000_get_unit_info(c_int16(self.handle), byref(s), c_int16(len(s)), c_int32(UNIT_INFO_TYPES[info]))
+            print(s.value.decode('utf-8'))
+
+    def close_scope(self):
+        lib.ps2000_close_unit(c_int16(self.handle))
+
+    def set_channel(self, channel, active, voltRange,coupling):
+        code = lib.ps2000_set_channel(c_int16(self.handle),c_int16(channelOpts[channel]), c_int16(active),c_int16(couplingOpts[coupling]),c_int16(voltRangeOpts[voltRange]))
+        if code ==0:
+            raise Exception("channel values out of range/error setting channel")
+        print("channel set successfully")
+        self.check_tb()
+        self.channelset= True
+
+    def check_tb(self):
+        self.tbs =[]
+        self.tis = []
+        self.mss = []
+        tb =1
+        cond = True
+        while cond:
+            ti = c_int32()
+            ms = c_int32()
+            code = lib.ps2000_get_timebase(c_int16(self.handle),c_int16(tb),c_void_p(),byref(ti),c_void_p(),c_int16(1),byref(ms))
+            if code==0:
+                cond = False
+            else:
+                self.tis.append(ti)
+                self.tbs.append(tb)
+                self.mss.append(ms)
+                tb+=1
+
+    def sig_gen(self):
+
+
+
+
+
+
+
+
+
+
+
+scope = ps2000()
+scope.dump_info()
+scope.set_channel("A",True,100,"DC")
+scope.close_scope()
+"""
+handle = lib.ps2000_open_unit()
+print(handle)
+info = "DriverVersion"
 lib.ps2000_flash_led(c_int16(handle))
 
 c = lib.ps2000_set_channel(c_int16(handle),c_int16(0), c_int16(1),c_int16(1),c_int16(6))
@@ -92,3 +157,4 @@ fig, ax  = plt.subplots()
 ax.plot(10**-9*dt*np.array(range(n)),dataA/32767)
 
 plt.savefig("test.png")
+    """
